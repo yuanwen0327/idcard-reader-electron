1feed306442abe48196807af28077d3823940d6f
yuanwen <yuanwen0327@outlook.com>
2021-03-18 16:48:29 +0800

    feat: 单例锁

--- main.js
+++ main.js
@@ -34,46 +34,53 @@ function createWindow() {
     win = null;
   });
 }
+const gotTheLock = app.requestSingleInstanceLock()
 
-app.setLoginItemSettings({
-  openAtLogin: true
-})
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.setLoginItemSettings({
+    openAtLogin: true,
+    args: ["--openAsHidden"]
+  })
 
-// Electron 会在初始化后并准备
-// 创建浏览器窗口时，调用这个函数。
-// 部分 API 在 ready 事件触发后才能使用。
-app.on('ready', () => {
-  tray = new Tray(path.join(__dirname, './assets/card.png'))
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: '退出', click: () => {
-        app.quit();
-      }
-    },
-  ])
-  tray.setToolTip('身份证阅读器')
-  tray.setContextMenu(contextMenu)
+  // Electron 会在初始化后并准备
+  // 创建浏览器窗口时，调用这个函数。
+  // 部分 API 在 ready 事件触发后才能使用。
+  app.on('ready', () => {
+    tray = new Tray(path.join(__dirname, './assets/card.png'))
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: '退出', click: () => {
+          app.quit();
+        }
+      },
+    ])
+    tray.setToolTip('身份证阅读器')
+    tray.setContextMenu(contextMenu)
 
-  // createWindow()
-  sdk.init()
-  io.init(8848)
-  // sdk.cardOn()
-  // sdk.read()
-});
+    // createWindow()
+    sdk.init()
+    io.init(8848)
+    // sdk.cardOn()
+    // sdk.read()
+  });
 
-// 当全部窗口关闭时退出。
-app.on('window-all-closed', () => {
-  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
-  // 否则绝大部分应用及其菜单栏会保持激活。
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+  // 当全部窗口关闭时退出。
+  app.on('window-all-closed', () => {
+    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+    // 否则绝大部分应用及其菜单栏会保持激活。
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
 
-app.on('activate', () => {
-  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
-  // 通常在应用程序中重新创建一个窗口。
-  if (win === null) {
-    createWindow();
-  }
-});
+  app.on('activate', () => {
+    // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+    // 通常在应用程序中重新创建一个窗口。
+    if (win === null) {
+      createWindow();
+    }
+  });
+}
+
